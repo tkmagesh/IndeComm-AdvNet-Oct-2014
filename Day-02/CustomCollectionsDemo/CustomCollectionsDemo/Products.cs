d00c44f88854bd04d27c590b8c5e0092e6a3e09a
tkmagesh <tkmagesh77@gmail.com>
2014-11-01 10:27:33 +0530

    Generics & Lambdas - Seed

---
+++ Day-02/CustomCollectionsDemo/CustomCollectionsDemo/Products.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomCollectionsDemo
+{
+    using System.Collections;
+
+    public class Products : IEnumerable, IEnumerator
+    {
+        private ArrayList _list = new ArrayList();
+
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+
+        public void Add(Product product)
+        {
+            _list.Add(product);
+        }
+
+        public Product Get(int index)
+        {
+            return (Product) _list[index];
+        }
+
+        public Product this[int index]
+        {
+            get
+            {
+                return (Product)_list[index];
+            }
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        private int _index = -1;
+        public bool MoveNext()
+        {
+            ++_index;
+            if (_index >= _list.Count)
+            {
+                Reset();
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        public object Current
+        {
+            get { return (Product) _list[_index]; }
+        }
+    }
+}
---
+++ Day-02/CustomCollectionsDemo/CustomCollectionsDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomCollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new Products();
+            products.Add(new Product { Id = 4, Name = "Pen", Category = 1, Units = 10, Cost = 50 });
+            products.Add(new Product { Id = 6, Name = "Hen", Category = 2, Units = 80, Cost = 30 });
+            products.Add(new Product { Id = 8, Name = "Ten", Category = 1, Units = 40, Cost = 80 });
+            products.Add(new Product { Id = 2, Name = "Den", Category = 2, Units = 50, Cost = 20 });
+            products.Add(new Product { Id = 9, Name = "Len", Category = 1, Units = 30, Cost = 10 });
+            products.Add(new Product { Id = 1, Name = "Zen", Category = 2, Units = 60, Cost = 70 });
+
+            Console.WriteLine("Default List..");
+            /*for(var i=0;i<products.Count;i++)
+                Console.WriteLine(products[i]);*/
+
+            foreach(var product in products)
+                Console.WriteLine(product);
+
+            Console.ReadLine();
+        }
+    }
+}
