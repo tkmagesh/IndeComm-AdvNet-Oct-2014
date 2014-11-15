b660447385943829c09049dc26198f5572473bb9
tkmagesh <tkmagesh77@gmail.com>
2014-11-15 11:15:40 +0530

    TPL - Seed

---
+++ Day-03/PrimeCountFinder/PrimeCountFinder/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimeCountFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindPrimes_Click(object sender, RoutedEventArgs e)
+        {
+            var start = 100000;
+            var end = 200000;
+            var t = Task.Factory.StartNew<int>((obj) =>
+            {
+                var result = GetPrimeCount(obj);
+                return result;
+            }, new PrimeFindData {Start = start, End = end});
+            t.ContinueWith(antecendent =>
+            {
+                MessageBox.Show(antecendent.Result.ToString());
+            });
+            MessageBox.Show("Work started");
+        }
+
+        private int GetPrimeCount(object data)
+        {
+            var primeFindData = (PrimeFindData) data;
+            var primeCount = 0;
+            for (var i = primeFindData.Start; i <= primeFindData.End; i++)
+            {
+                if (isPrime(i))
+                    primeCount++;
+            }
+            return primeCount;
+        }
+
+        private bool isPrime(int n)
+        {
+            for(var i=2;i<=(n/2);i++)
+                if (n%i == 0) return false;
+            return true;
+        }
+    }
+
+    public class PrimeFindData
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
---
+++ Day-03/TPLDemo/TPLDemo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TPLDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoWork_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Work Started..");
+            var t = new Task(DoWork);
+            TaskAwaiter taskAwaiter = t.GetAwaiter();
+            
+            t.ContinueWith((t2) =>
+            {
+                MessageBox.Show("Work Completed..");
+            });
+            t.Start();
+            
+        }
+
+        private void DoWork()
+        {
+            for(var i=0;i<10000;i++)
+                for(var j=0;j<10000;j++)
+                    for(var k=0;k<100;k++){}
+            Debug.WriteLine("Task actual completion..!");
+        }
+    }
+}
